09273da7646382fc58ffcc1ec6da180aa8bf95fe
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-24 01:46:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,33 +60,52 @@ def current_player
   return player
 end
 
-def turn
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(index)
-    player_token = current_player
-    move(index, player_token)
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  current=current_player(board)
+
+  if valid_move? (index)
+    move( index,current)
     display_board
   else
+    puts "try it again"
     turn
+  end
+  
+end
+
+def winner
+  index = []
+  index = won?
+  if index == false
+    return nil
+  else
+    if @board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
 
+
+
 def won?
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each {|wc|
+    win_index_1 = wc[0]
+    win_index_2 = wc[1]
+    win_index_3 = wc[2]
 
-    position_1 = @board[index_0]
-    position_2 = @board[index_1]
-    position_3 = @board[index_2]
+    p1 = @board[win_index_0]
+    p1 = @board[win_index_1]
+    p1 = @board[win_index_2]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+    if p1 == "X" && p2 == "X" && p3 == "X"
+      return wc
+    elsif p1 == "O" && p2 == "O" && p3 == "O"
+      return wc
     end
   }
   return false
@@ -112,19 +131,7 @@ def over?
   end
 end
 
-def winner
-  index = []
-  index = won?
-  if index == false
-    return nil
-  else
-    if @board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
-end
+
 
 def play
   until over? == true
