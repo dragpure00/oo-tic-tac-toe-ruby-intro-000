42b5cb4ca21763dd57da2eec1bd2756fd10b15f5
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-24 01:17:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,22 +1,15 @@
 class TicTacToe
-WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
 
-[0,3,6],
-[1,4,7],
-[2,5,8],
-
-[0,4,8],
-[2,4,6]
-
-
-]
-
-
-
-
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
 
 def initialize
   @board = Array.new(9, " ")
@@ -29,17 +22,17 @@ def display_board
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
-def input_to_index(current)
-  new_input=current.to_i
-  return new_input -= 1
+
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
-def move(index,character)
-  return @board[index]=character
+def move(index, current_player = "X")
+  @board[index] = current_player
 end
 
-def position_taken?( index)
-  @board[index] != " "
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
 end
 
 def valid_move?(index)
@@ -47,76 +40,58 @@ def valid_move?(index)
 end
 
 def turn_count
-   counter = 0
-   @board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
 end
 
 def current_player
-  turn = turn_count
-  if(turn %2 ==0)
-    return "X"
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
   else
-    return "O"
+    player = "O"
   end
+  return player
 end
 
 def turn
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  current=current_player
-
-  if valid_move?( index)
-    move(index,current)
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
     display_board
   else
-    puts "try it again"
     turn
   end
 end
 
-def winner(board)
-  index=[]
-  index=won?
-  if index==false
-    return nil
-  else
-    if @board[index[0]]== "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
-end
-
-
-
-
-
-
 def won?
-  WIN_COMBINATIONS.each {|wc|
-    win_index_1 = wc[0]
-    win_index_2 = wc[1]
-    win_index_3 = wc[2]
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-    p1= @board[win_index_1]
-    p2 = @board[win_index_1]
-    p3 = @board[win_index_3]
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
 
-    if p1== "X" && p2 == "X" && p3 == "X"
-      return wc
-    elsif p1 == "O" && p2 == "O" && p3 == "O"
-      return wc
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
   }
   return false
 end
+
 def full?
   @board.all? {|index| index == "X" || index == "O"}
 end
@@ -137,9 +112,19 @@ def over?
   end
 end
 
-
-
-
+def winner
+  index = []
+  index = won?
+  if index == false
+    return nil
+  else
+    if @board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
 
 def play
   until over? == true
@@ -153,5 +138,4 @@ def play
   end
 end
 
-end
 end
